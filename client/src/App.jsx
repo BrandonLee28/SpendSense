91199f70de688872c1e73856b7859abc782cb466
Brandon <76419269+BrandonLee28@users.noreply.github.com>
2023-08-15 14:58:21 -0400

    fixed baseurl for api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Signup from './pages/Signup'
-import Budgets from './pages/Budgets'
-import axios from 'axios' 
-import Logout from './pages/Logout'
-import Login from './pages/Login'
-import Home from './pages/Home'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Budgets from "./pages/Budgets";
+import axios from "axios";
+import Logout from "./pages/Logout";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 const App = () => {
-  axios.defaults.baseURL = 'http://localhost:3000';
-  axios.defaults.withCredentials = true
-
+  axios.defaults.baseURL = "https://pleasant-red-turtleneck.cyclic.cloud";
+  axios.defaults.withCredentials = true;
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup/>}></Route>
-        <Route path="/budgets/:budgetId" element={<Budgets/>}></Route> 
-        <Route path="/logout" element={<Logout/>}></Route> 
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/home" element={<Home/>}></Route> 
+        <Route path="/" element={<Signup />}></Route>
+        <Route path="/budgets/:budgetId" element={<Budgets />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
